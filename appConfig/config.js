bac4e292d613764ebb9e475c63960495697b411f
Vivek Kushwaha <vvkkushwaha@outlook.com>
2018-12-06 16:10:58 +0530

    fixed config

--- appConfig/config.js
+++ appConfig/config.js
@@ -1,6 +1,4 @@
 export default ({
-  API_URL: 'http://www.paysalaryday.com:7070/SASystem/',
-  GEO_API: 'AIzaSyBRilOyqCZQ47WV7hyS7ROUTfnZKfeKnd8',
   APP_NAME: 'SITE ATTENDANCE',
   GEO_API_URL: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
   REPORT_PATH:'http://192.168.0.101/testpage1'
