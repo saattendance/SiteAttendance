55d579fcb500e8d6ce618190b56b4b8f3a1f6d0e
Vivek Kushwaha <vvkkushwaha@outlook.com>
2018-12-06 16:23:46 +0530

    removed report

--- appConfig/config.js
+++ appConfig/config.js
@@ -1,5 +1,4 @@
 export default ({
   APP_NAME: 'SITE ATTENDANCE',
-  GEO_API_URL: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
-  REPORT_PATH:'http://192.168.0.101/testpage1'
+  GEO_API_URL: 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
 });
